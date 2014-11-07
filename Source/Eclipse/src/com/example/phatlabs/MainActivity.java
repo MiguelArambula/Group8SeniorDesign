306cbef476fb5c0a1ebd94073da1cf21928fdad3
Binsk <binskified@gmail.com>
2014-11-07 13:11:06 -0700

    Created class for saving preference data onto the SD card

--- Source/Eclipse/src/com/example/phatlabs/MainActivity.java
+++ Source/Eclipse/src/com/example/phatlabs/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.phatlabs;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +11,21 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+	
+		ExternalData file=new ExternalData();
+		file.openw("test");
+		file.openr("test");
+		
+		file.writeKey("entry", "this is a value");
+		String str = file.readKey("entry");
+
+		if (str == null)
+			Log.i("Phat Lab","Failed to read");
+		else
+			Log.i("Phat Lab",str);
+		
+		file.closer();
+		file.closew();
 	}
 }
