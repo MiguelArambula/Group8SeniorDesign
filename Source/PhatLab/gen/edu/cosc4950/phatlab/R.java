6f969ffa9908032332e1b61df44bee6339f945bc
MiguelArambula <marambul@uwyo.edu>
2015-01-30 12:41:47 -0700

    remove more stuff
    Remove more un-needed items

--- Source/PhatLab/gen/edu/cosc4950/phatlab/R.java
+++ Source/PhatLab/gen/edu/cosc4950/phatlab/R.java
@@ -28,13 +28,12 @@ public final class R {
         public static final int pad_pressed=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080013;
+        public static final int action_settings=0x7f080012;
         public static final int activity_main_console_container=0x7f080001;
         public static final int activity_main_phat_pad_container=0x7f080000;
         public static final int changeSamp=0x7f080003;
         public static final int current=0x7f080004;
         public static final int edit=0x7f080002;
-        public static final int file_text=0x7f080012;
         public static final int pad01=0x7f080006;
         public static final int pad02=0x7f080007;
         public static final int pad03=0x7f080008;
@@ -53,8 +52,6 @@ public final class R {
         public static final int activity_main=0x7f030000;
         public static final int fragment_console=0x7f030001;
         public static final int fragment_phatpad=0x7f030002;
-        public static final int list_items=0x7f030003;
-        public static final int view_empty=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
