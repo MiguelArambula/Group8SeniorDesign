c566d00765c36fce19d18a4f6e02aebd7ad2870f
MiguelArambula <marambul@uwyo.edu>
2015-02-06 07:45:08 -0700

    File Changer
    Finally got the start of the file changer, just need to add the methods
    and clean it up a bit.

--- Source/PhatLab/src/edu/cosc4950/phatlab/ConsoleFragment.java
+++ Source/PhatLab/src/edu/cosc4950/phatlab/ConsoleFragment.java
@@ -1,5 +1,7 @@
 package edu.cosc4950.phatlab;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
@@ -8,6 +10,7 @@ import android.content.Intent;
 import android.graphics.Point;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -15,6 +18,9 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ListView;
@@ -39,27 +45,46 @@ public class ConsoleFragment extends Fragment {
 		
 		View myView = inflater.inflate(R.layout.fragment_console, container, false);
 		//cv = (ConsoleView) myView.findViewById(R.id.consoleView1);
+
+		root = Environment.getExternalStorageDirectory()+"/PhatLab/";
+		getDir(root);
+		final Spinner spin = (Spinner) myView.findViewById(R.id.spin);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>
+		(getActivity(), android.R.layout.simple_spinner_item, item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spin.setAdapter(adapter);
+		spin.setOnItemSelectedListener(new OnItemSelectedListener(){
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id) {
+				spin.setSelection(position);
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 		final ToggleButton edit = (ToggleButton) myView.findViewById(R.id.edit);
-		//final Button changeSamp = (Button) myView.findViewById(R.id.changeSamp);
 		edit.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
 
 			@Override
 			public void onCheckedChanged(CompoundButton buttonView,
 					boolean isChecked) {
-				// TODO Auto-generated method stub
-				//changeSamp.setEnabled(isChecked);
+				if(isChecked){
+					spin.setEnabled(true);
+					spin.setSelection(0);
+				} else {
+					spin.setEnabled(false);
+				}
 			}
 			
 		});
-		/*changeSamp.setOnClickListener(new OnClickListener(){
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-			}
-			
-		});*/
-		final Spinner spin = (Spinner) myView.findViewById(R.id.spin);
 		final AudioManager manager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
 		final SeekBar volume = (SeekBar) myView.findViewById(R.id.volume);
 		volume.setMax(manager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
@@ -86,7 +111,37 @@ public class ConsoleFragment extends Fragment {
 			}
 			
 		});
+		
+		
 		return myView;
+	}
+	
+	private void getDir(String dirPath){
+		//myPath.setText("Location"  + dirPath);
+		item = new ArrayList<String>();
+		path = new ArrayList<String>();
+		File f = new File(dirPath);
+		File[] files = f.listFiles();
+		
+		if(!dirPath.equals(root)){
+			item.add(root);
+			path.add(root);
+			item.add("../");
+			path.add(f.getParent());
+		}
+		
+		for(int i=0;i<files.length;i++){
+			File temp = files[i];
+			
+			if(!temp.isHidden() && temp.canRead()){
+				path.add(temp.getPath());
+				if(temp.isDirectory()){
+					item.add(temp.getName()+"/");
+				} else {
+					item.add(temp.getName());
+				}
+			}
+		}
 	}
 	
 	public void onToggleClicked(View view, Button button){
