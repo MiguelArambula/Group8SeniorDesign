8a99258f7289753b4ab7d930400e43e4a24dcfaa
jharper6 <jharper6@uwyo.edu>
2015-02-21 13:25:48 -0700

    Integrated PhatConsole code from Miguel.

--- Source/PhatLab/src/edu/cosc4950/phatlab/ConsoleFragment.java
+++ Source/PhatLab/src/edu/cosc4950/phatlab/ConsoleFragment.java
@@ -1,60 +1,95 @@
 package edu.cosc4950.phatlab;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnDragListener;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
-public class ConsoleFragment extends Fragment {
+public class ConsoleFragment extends Fragment{
 	
 	ConsoleView cv;
 	static boolean editOn;
+	private String root;
 	
-	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		
+		final MainActivity data = (MainActivity) getActivity();
 		View myView = inflater.inflate(R.layout.fragment_console, container, false);
-		//cv = (ConsoleView) myView.findViewById(R.id.consoleView1);
+		root = Environment.getExternalStorageDirectory()+"/PhatLab/";
+		data.getDir(root);
+		
+		final Spinner spin = (Spinner) myView.findViewById(R.id.spin);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>
+		(getActivity(), android.R.layout.simple_spinner_item, data.getItems());
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spin.setAdapter(adapter);
+		spin.setOnItemSelectedListener(new OnItemSelectedListener(){
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				spin.setFocusable(true);
+				spin.setSelection(position);
+				//Toast.makeText(getActivity(), String.valueOf(spin.getSelectedItem()), Toast.LENGTH_SHORT).show();
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		spin.setEnabled(false);
 		final ToggleButton edit = (ToggleButton) myView.findViewById(R.id.edit);
-		final Button changeSamp = (Button) myView.findViewById(R.id.changeSamp);
 		edit.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
 
 			@Override
 			public void onCheckedChanged(CompoundButton buttonView,
 					boolean isChecked) {
-				// TODO Auto-generated method stub
-				changeSamp.setEnabled(isChecked);
-			}
-			
-		});
-		changeSamp.setOnClickListener(new OnClickListener(){
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-
-				
+				if(isChecked){
+					spin.setEnabled(true);
+				} else {
+					spin.setEnabled(false);
+				}
 			}
 			
 		});
 		final AudioManager manager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
-		final SeekBar volume = (SeekBar) myView.findViewById(R.id.volume);
+		final SeekBar volume = (SeekBar) myView.findViewById(R.id.volumebar);
 		volume.setMax(manager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
 		volume.setProgress(manager.getStreamVolume(AudioManager.STREAM_MUSIC));
 		volume.setOnSeekBarChangeListener(new OnSeekBarChangeListener(){
@@ -63,37 +98,50 @@ public class ConsoleFragment extends Fragment {
 			public void onProgressChanged(SeekBar seekBar, int progress,
 					boolean fromUser) {
 				// TODO Auto-generated method stub
+				seekBar.setProgress(progress);
 				manager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
 			}
 
 			@Override
 			public void onStartTrackingTouch(SeekBar seekBar) {
 				// TODO Auto-generated method stub
-				
+				int x = seekBar.getProgress();
+				seekBar.setProgress(x);
 			}
 
 			@Override
 			public void onStopTrackingTouch(SeekBar seekBar) {
 				// TODO Auto-generated method stub
-				
+				int x = seekBar.getProgress();
+				seekBar.setProgress(x);
+			}
+		});
+		final TextView maxText = (TextView) myView.findViewById(R.id.max_text);
+		maxText.setText(Integer.toString(data.getMaxBeat()));
+		final Button decMax = (Button) myView.findViewById(R.id.dec_max);
+		decMax.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				int x = data.changeMax("sub");
+				maxText.setText(Integer.toString(x));
 			}
 			
 		});
-		
-		
-		
+		final Button addMax = (Button) myView.findViewById(R.id.in_max);
+		addMax.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				int x = data.changeMax("add");
+				maxText.setText(Integer.toString(x));
+			}
+			
+		});
+		final TextView currPad = (TextView) myView.findViewById(R.id.pad_num);
+		currPad.setText(data.getCurrPad());
 		return myView;
 	}
-	
-	public void onToggleClicked(View view, Button button){
-		//Checks to see if EDIT has been pressed
-		boolean on = ((ToggleButton) view).isChecked();
-		
-		if(on){
-			editOn=true;
-		} else {
-			editOn=false;
-		}
-	}
-	
 }
--- Source/PhatLab/src/edu/cosc4950/phatlab/MainActivity.java
+++ Source/PhatLab/src/edu/cosc4950/phatlab/MainActivity.java
@@ -1,8 +1,14 @@
 package edu.cosc4950.phatlab;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -16,11 +22,19 @@ import android.os.Build;
 
 public class MainActivity extends FragmentActivity {
 	
+	private ViewGroup mPadLayout, mConsoleLayout;
+	private ExternalData data = new ExternalData();
+	private Point p;
+	private List<String> item = null;
+	private List<String> path = null;
+	private String root=Environment.getExternalStorageDirectory()+"/PhatLab/";
+	
 	SequenceTimer sequence; 
 	int maxBeat, currentBeat;
 	
-	private ViewGroup mPadLayout, mConsoleLayout;
-
+	String currPad, currSamp;
+	boolean c=false;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		
@@ -43,14 +57,14 @@ public class MainActivity extends FragmentActivity {
 		if (savedInstanceState == null) {
 
 			mPadLayout = (ViewGroup) findViewById(R.id.activity_main_phat_pad_container);
-			if(mPadLayout != null) {
+			/*if(mPadLayout != null) {
 				
 				/*
 				PhatPadFragment phatPadFragment = new PhatPadFragment();
 				FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
 				fragmentTransaction.replace(mPadLayout.getId(), phatPadFragment, PhatPadFragment.class.getName());
 				
-				fragmentTransaction.commit();*/
+				fragmentTransaction.commit();* /
 				
 				
 				// THIS IS TEST CODE. USE THE STUFF ABOVE ^^^
@@ -59,17 +73,17 @@ public class MainActivity extends FragmentActivity {
 				fragmentTransaction.replace(mPadLayout.getId(), sequencerFragment, SequencerFragment.class.getName());
 				
 				fragmentTransaction.commit();
-			}
+			}*/
 			
 			mConsoleLayout = (ViewGroup) findViewById(R.id.activity_main_console_container);
-			if(mConsoleLayout != null) {
+			/*if(mConsoleLayout != null) {
 				
 				ConsoleFragment consoleFragment = new ConsoleFragment();
 				FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
 				fragmentTransaction.replace(mConsoleLayout.getId(), consoleFragment, ConsoleFragment.class.getName());
 				
 				fragmentTransaction.commit();
-			}
+			}*/
 		}
 	}
 
@@ -102,4 +116,74 @@ public class MainActivity extends FragmentActivity {
 	public void onDestroy() {
 		super.onDestroy();
 	}	
+	
+	public void getDir(String dirPath){
+		//myPath.setText("Location"  + dirPath);
+		item = new ArrayList<String>();
+		item.add("");
+		path = new ArrayList<String>();
+		File f = new File(dirPath);
+		File[] files = f.listFiles();
+		
+		if(!dirPath.equals(root)){
+			item.add(root);
+			path.add(root);
+			item.add("../");
+			path.add(f.getParent());
+		}
+		
+		for(int i=0;i<files.length;i++){
+			File temp = files[i];
+			
+			if(!temp.isHidden() && temp.canRead()){
+				path.add(temp.getPath());
+				if(temp.isDirectory()){
+					item.add(temp.getName()+"/");
+				} else {
+					String wav = temp.getName();
+					wav = wav.substring(0, wav.length()-4);
+					if(data.isValidWav(wav)){
+					item.add(wav);
+					}
+				}
+			}
+		}
+	}
+	
+	public List<String> getItems(){
+		return item;
+	}
+	
+	public int getMaxBeat(){
+		return maxBeat;
+	}
+	
+	public int changeMax(String c){
+		if(c=="add"){
+			maxBeat += 1;
+		} else if(c=="sub"){
+			if(maxBeat>1){
+				maxBeat -= 1;
+			} else {
+				maxBeat += 0;
+			}
+		}
+		return maxBeat;
+	}
+	
+	public void setCurrPad(String s){
+		currPad = s;
+	}
+	
+	public void setCurrSamp(String s){
+		currSamp = s;
+	}
+	
+	public String getCurrPad(){
+		return currPad;
+	}
+	
+	public String getCurrSamp(){
+		return currSamp;
+	}
 }
