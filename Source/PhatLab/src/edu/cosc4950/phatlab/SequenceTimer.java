a29f1e8b4028815193206a2dafd03e50b59f21e4
binsk <binskified@gmail.com>
2015-02-27 20:16:12 -0700

    Added PCM compilation from sequencer

--- Source/PhatLab/src/edu/cosc4950/phatlab/SequenceTimer.java
+++ Source/PhatLab/src/edu/cosc4950/phatlab/SequenceTimer.java
@@ -63,7 +63,7 @@ public class SequenceTimer
 	{
 		return findTrigger(null, track, beat, step);
 	}
-	
+
 	/**
 	 * Finds a node in a track if it exists. Returns null if it does not.
 	 * @param startNode	The node to start searching at. null starts at the beginning
@@ -238,16 +238,19 @@ public class SequenceTimer
 							if (curPos >= endPos)
 							{
 								stop();
+								isPlaying = false;
 								break;
 							}
 							//Log.i("Phat Lab", "Step: "+curPos + ": "+endPos);
 							++ curPos;
 							Thread.sleep(60000 / ((bpm * spb) / 4),0);
 						}
+						isPlaying = false;
 					}
 					catch (Exception e)
 					{
 						Log.e("Phat Lab","Error while playing sequence: ", e);
+						isPlaying = false;
 						stop();
 					}
 				}
@@ -274,10 +277,77 @@ public class SequenceTimer
 	 */
 	public PCM compileToPCM()
 	{
-		// -- STUB -- //
-		return null;
+		try
+		{
+			/*
+			 *  The *16 at the end is:
+			 *  2 (stereo) * 2 (bytes per sample) * 4 (quarter notes per beat)
+			 */
+			int totalBytes = (int) Math.ceil((1.0 / (double)(bpm * spb) * (double)totalSteps) * 60) * 44100 * 16;
+			//Get number of seconds, multiply by sample rate, then by 2 for stereo
+			int longestSample = 0;
+			for (int i = 0; i < 12; ++i)
+			{
+				if (triggerList[i] == null)
+					continue;
+				if (sampleList[i] == null)
+					continue;
+				
+				if (sampleList[i].getStream().length * (sampleList[i].getStereo()?2:1) > longestSample)
+					longestSample = sampleList[i].getStream().length * (sampleList[i].getStereo()?2:1);
+			}
+			totalBytes += longestSample; // Add the length of the longest sample to the end
+			//Create and clear the new byte data
+			byte[] sampleBytes = new byte[totalBytes];
+			for (int i = 0; i < sampleBytes.length; ++i)
+				sampleBytes[i] = 0;
+			
+			for (int i = 0; i < 12; ++i)
+			{
+				//If no sample on this track, just continue:
+				if (triggerList[i] == null)
+					continue;
+				if (sampleList[i] == null)
+					continue;
+				
+				sNode 	first = triggerList[i].findFirst();
+				byte[] samples = sampleList[i].getStream();
+				
+				//Loop through all samples
+				for (sNode j = first; j != null; j = j.next)
+				{
+					//Byte position to start at:
+					int bp = (int)((1.f / (double)(bpm * spb) * (double)j.priority ) * 60.f * 44100.f * 16.f);
+					
+					//If stereo, we just copy it directly:
+					if (sampleList[i].getStereo())
+					{
+						//Log.i("Phat Lab", "Stereo");
+						for (int k = 0; k < samples.length; ++k)
+							sampleBytes[bp + k] += samples[k];
+					}
+					//If not stereo, we copy the mono into both channels:
+					else
+					{
+						//Log.i("Phat Lab", "Mono");
+						for (int k = 0; k < samples.length; ++k)
+						{
+							sampleBytes[bp + (2*k)] = samples[k];
+							sampleBytes[bp + (2*k + 1)] = samples[k];
+						}
+					}
+				}
+			}
+			
+			PCM pcm = new PCM(sampleBytes, 44100 ,true);
+			return pcm;
+		}
+		catch (Exception e)
+		{
+			Log.e("Phat Lab", "Error generating audio sequence!",e);
+			return null;
+		}
 	}
-	
 }
 
 
@@ -378,6 +448,22 @@ class sNode
 				curNode = curNode.next;
 			}
 		}
+	}
+	
+	public sNode findFirst()
+	{
+		if (prev == null)
+			return this;
+		else
+			return prev.findFirst();
+	}
+	
+	public sNode findLast()
+	{
+		if (next == null)
+			return this;
+		else
+			return next.findLast();
 	}
 	
 	public sNode clear()
